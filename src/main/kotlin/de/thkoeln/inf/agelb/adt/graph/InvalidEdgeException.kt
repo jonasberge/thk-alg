d6fed14522c85a429e47596c8b0ec799798d7525
ai1789 <>
2019-11-15 00:59:53 +0100

    Unser Repo etwas umstrukturiert

--- src/main/kotlin/de/thkoeln/inf/agelb/adt/graph/InvalidEdgeException.kt
+++
@@ -1,4 +0,0 @@
-package de.thkoeln.inf.agelb.adt.graph
-
-class InvalidEdgeException(message: String? = null)
-    : Exception(message)
--- src/main/kotlin/de/thkoeln/inf/agelb/live/Main.kt
+++
@@ -1,5 +0,0 @@
-package de.thkoeln.inf.agelb.live
-
-fun main() {
-    println("Hello TH-Köln! Live at Coding Jam!")
-}
